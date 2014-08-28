6b15d01f28a10a292072fab8ad5bc43830358170
Kevin Kelly <kevin.kelly@axiosengineering.com>
2014-08-28 12:42:43 -0400

    Renamed piwik for build order

--- dist/js/ozpIwc-client.js
+++ dist/js/ozpIwc-client.js
@@ -2230,30 +2230,6 @@ ozpIwc.util.ajax = function (config) {
         request.send();
     });
 };
-var _paq = _paq || [];
-
-// Custom Variable Tracking
-_paq.push(['setCustomVariable',
-    // Index, the number from 1 to 5 where this custom variable name is stored
-    1,
-    // Name, the name of the variable, for example: Gender, VisitorType
-    "VisitorType",
-    // Value, for example: "Male", "Female" or "new", "engaged", "customer"
-    "Member",
-    // Scope of the custom variable, "visit" means the custom variable applies to the current visit
-    "visit"
-]);
-
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://10.10.10.145/webapps/piwik/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 5]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
-    g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-})();
-console.log(this);
 var ozpIwc=ozpIwc || {};
 
 //TODO get these from the api registry when available
@@ -2582,4 +2558,28 @@ ozpIwc.Client.prototype.setApiInfo=function(apiName,last) {
         return p;
     }
 })();
+var _paq = _paq || [];
+
+// Custom Variable Tracking
+_paq.push(['setCustomVariable',
+    // Index, the number from 1 to 5 where this custom variable name is stored
+    1,
+    // Name, the name of the variable, for example: Gender, VisitorType
+    "VisitorType",
+    // Value, for example: "Male", "Female" or "new", "engaged", "customer"
+    "Member",
+    // Scope of the custom variable, "visit" means the custom variable applies to the current visit
+    "visit"
+]);
+
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://10.10.10.145/webapps/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 5]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+    g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
+console.log(this);
 //# sourceMappingURL=ozpIwc-client.js.map
